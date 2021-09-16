b5e79a04641e09e80a44611596851e45d5577e10
Pei Ern Koh <peiern.28@gmail.com>
2021-09-16 11:29:36 +0800

    added favorites icon to index and show

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,7 @@
+class FavoritesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
---
+++ test/controllers/favorites_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class FavoritesControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get favorites_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get favorites_create_url
+    assert_response :success
+  end
+
+end
