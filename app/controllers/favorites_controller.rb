3aa1e9bb93609d198dbcf9e30d41aa855eb298a8
Pei Ern Koh <peiern.28@gmail.com>
2021-09-16 12:27:24 +0800

    able to favorite in show

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,5 +3,13 @@ class FavoritesController < ApplicationController
   end
 
   def create
+    @favorite = Favorite.new
+    @favorite.user = current_user
+    @favorite.experience = Experience.find(params[:experience_id])
+    if @favorite.save
+      respond_to do |format|
+        format.json { render json: { status: "favorite successfully created" } }
+      end
+    end
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,9 +26,10 @@ require("channels")
 import "bootstrap";
 
 // Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+import { initFavorites } from '../plugins/init_favorites';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initFavorites();
 });
