fb8f4358ec936f083f857c55479bece13bffb4fe
Ashton Pasumbal <ashton.shane.dev@gmail.com>
2021-09-16 12:46:22 +0800

    add functionality where multi-word search terms will be searched individually and results of each of those terms will be returned

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,7 +1,11 @@
 class ExperiencesController < ApplicationController
   def index
     if params[:query].present?
-      @experiences = Experience.search_by_name_and_details(params[:query])
+      query = params[:query].split # Split query into an array of terms
+      @experiences = []
+      query.each do |term|
+        results = Experience.search_by_name_and_details(term) # do a search for each of those terms individually
+        @experiences.concat(results) # add each of those search results to experiences to experiences array and flatten array
     else
       @experiences = Experience.all
     end
