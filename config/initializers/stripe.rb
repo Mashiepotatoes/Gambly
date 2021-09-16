fbaf3b8434327b32a5f20873d988fc7aa1542f65
Jeremy Lim <limjeremy29@gmail.com>
2021-09-16 16:11:47 +0800

    created stripe ruby file

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -5,4 +5,9 @@ Rails.configuration.stripe = {
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
 StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
+
+StripeEvent.configure do |events|
+  events.subscribe 'checkout.session.completed', StripeCheckoutSessionService.new
+end
