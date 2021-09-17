c084b228b6203282688d64b14d70d7d10140f4ba
Pei Ern Koh <peiern.28@gmail.com>
2021-09-17 19:06:28 +0800

    able to update cart

--- app/javascript/plugins/init_Modal.js
+++ app/javascript/plugins/init_Modal.js
@@ -12,27 +12,32 @@ const initModal = () => {
       modal.find('.modal-title').text(experienceName)
       modal.find('.modal-price').text('$' + experiencePrice)
       modal.attr('data-current-experience-id', experienceId)
+<<<<<<< HEAD
+=======
+      prefillInputs(modal, experienceId);
+>>>>>>> e855def (able to update cart)
     })
     initAddToCart();
   }
 }
 
-// const initAddToCart = () => {
-//   // select the modal button
-//   const AddToCartBtn = document.querySelector('.btn-modal');
-//   AddToCartBtn.addEventListener('click', () => {
-//     const currentExperienceId = document.querySelector('.modal').dataset.currentExperienceId;
-//     const pax = document.querySelector('#experience-pax').value;
+const initAddToCart = () => {
+  // select the modal button
+  const addToCartBtn = document.querySelector('.btn-modal');
+  addToCartBtn.addEventListener('click', () => {
+    const currentExperienceId = document.querySelector('.modal').dataset.currentExperienceId;
+    const pax = document.querySelector('#experience-pax').value;
 
-//     const experienceOrder = {
-//       experienceId: currentExperienceId,
-//       pax: pax
-//     }
+    const experienceOrder = {
+      experienceId: currentExperienceId,
+      pax: pax
+    }
 
-//     writeToLocalStorage(experienceOrder)
-//   })
-// }
+    writeToLocalStorage(experienceOrder)
+  })
+}
 
+<<<<<<< HEAD
 const findExperienceInCart = (experienceOrder, orderArr) => {
   debugger
   return orderArr.find((element) => element.experienceId === experienceOrder);
@@ -60,15 +65,65 @@ const writeToLocalStorage = (experienceOrder) => {
   }
   // debugger
   console.log(window.localStorage.order);
+=======
+  const findExperienceInCart = (experienceOrder, orderArr) => {
+    return orderArr.find((element) => element.experienceId === experienceOrder);
+  }
 
-//   //when i click on it
+  const writeToLocalStorage = (experienceOrder) => {
+    // if we do have a key for storing our items
+    // add the experience order directly to it
+    if (window.localStorage.order) {
+      const orderArr = JSON.parse(window.localStorage.order);
+      const experienceInCart = findExperienceInCart(experienceOrder.experienceId, orderArr);
+      // if the experienceOrder.id is in the order
+      if (experienceInCart) {
+        // then update
+        experienceInCart.amount = experienceOrder.amount;
+        window.localStorage.order = JSON.stringify(orderArr);
+      } else {
+        const orderArr = JSON.parse(window.localStorage.order);
+        orderArr.push(experienceOrder);
+        window.localStorage.order = JSON.stringify(orderArr);
+      }
+    } else {
+      window.localStorage.order = JSON.stringify([experienceOrder])
+      // if we dont have it, start it and add the item
+    }
+    // debugger
+    console.log(window.localStorage.order);
 
-//   // get the id from the modal div - current experience
-//   // get the amt from the form
+  }
+>>>>>>> e855def (able to update cart)
+
+  //when i click on it
+
+  // get the id from the modal div - current experience
+  // get the amt from the form
 
   // write to local storage
+<<<<<<< HEAD
 }
 
 
 
+=======
+
+  const prefillInputs = (modal, experienceId) => {
+    if (window.localStorage.order) {
+      const orderArr = JSON.parse(window.localStorage.order);
+      const experienceInCart = findExperienceInCart(experienceId.toString(), orderArr);
+      if (experienceInCart) {
+        modal.find('#experience-pax').val(experienceInCart.amount)
+      } else {
+        modal.find('#experience-pax').val('')
+      }
+    }
+  }
+    // if the experienceid is in the localstorage
+    // update the modal inputs with the content
+    // otherwise
+    // the inputs should be empty
+
+>>>>>>> e855def (able to update cart)
 export { initModal }
