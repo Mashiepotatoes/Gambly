e4a10c822635115d42012d22a76a13a3b5345562
Jeremy Lim <limjeremy29@gmail.com>
2021-09-17 12:09:09 +0800

    update the display of price

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -4,7 +4,7 @@ class Experience < ApplicationRecord
   has_many :users, through: :user_experiences
   validates :name, presence: true, length: { minimum: 1 }
   validates :details, presence: true
-  validates :price_cents, presence: true, numericality: { greater_than: 0, less_than: 1000, allow_blank: true }
+  # validates :price_cents, presence: true, numericality: { greater_than: 0, less_than: 1000, allow_blank: true }
 
   monetize :price_cents
 
