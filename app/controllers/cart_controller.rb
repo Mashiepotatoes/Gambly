cf86f108b1b13e3376b6378d40734f568b21f361
Pei Ern Koh <peiern.28@gmail.com>
2021-09-19 19:31:47 +0800

    added codes to retrieve data from server

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,4 +1,20 @@
 class CartController < ApplicationController
   def show
   end
+
+  def cart_info
+    total = 0
+    experience_params.map do |item|
+      experience = Experience.find(item['experienceId'])
+      total += experience.price * item['amount'].to_i
+      item.merge!({ experiencePrice: experience.price, experienceName: experience.name })
+    end
+    respond_to do |format|
+      format.json { render json: { items: experience_params, total: total } }
+    end
+  end
+
+  def experience_params
+    params.permit!["_json"]
+  end
 end
--- app/javascript/plugins/init_Cart.js
+++ app/javascript/plugins/init_Cart.js
@@ -1,13 +1,34 @@
+import { csrfToken } from "@rails/ujs"
+
 const initCart = () => {
   const cart = document.querySelector('#cart');
   if (cart) {
     const noItems = document.querySelector('.no-items');
     if (window.localStorage.order) {
       cart.classList.remove('d-none');
+      fetch('/cart_info', {
+        method: 'POST',
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+          "X-CSRF-Token": csrfToken()
+        },
+        body: window.localStorage.order
+      }).then(res => res.json())
+        .then(data => renderCartItems(data, cart));
     } else {
       noItems.classList.remove('d-none');
     }
   }
+}
+
+const renderCartItems = (data, cart) => {
+  const cartItems = cart.querySelector('.cart-items');
+  data.items.forEach((item) => {
+    cartItems.innerHTML += `
+    <p>${item.experienceName} - Price: ${item.experiencePrice}</p>`
+  })
+  cartItems.insertAdjacentHTML('beforeend', `<p> Total: ${data.total} <p>`)
 }
 
 export { initCart }
