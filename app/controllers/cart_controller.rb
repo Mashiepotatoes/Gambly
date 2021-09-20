984f8139cac234c6968f6cc34fa747a0202efdab
Pei Ern Koh <peiern.28@gmail.com>
2021-09-20 10:45:13 +0800

    price is showing in cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -4,10 +4,10 @@ class CartController < ApplicationController
 
   def cart_info
     total = 0
-    experience_params.map do |item|
+    experience_params.each do |item|
       experience = Experience.find(item['experienceId'])
-      total += experience.price * item['amount'].to_i
-      item.merge!({ experiencePrice: experience.price, experienceName: experience.name })
+      total += (experience.price_cents / 100) * item['pax'].to_i
+      item.merge!({ experiencePrice: experience.price_cents, experienceName: experience.name })
     end
     respond_to do |format|
       format.json { render json: { items: experience_params, total: total } }
